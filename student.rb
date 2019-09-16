30d08157b1fd251505187498406d4ad849e6ab8a
mjpayne8 <markpayne@Marks-MacBook-Air.local>
2019-09-16 13:51:01 +0100

    setter methods added, tests passed

--- student.rb
+++ student.rb
@@ -1,8 +1,8 @@
 class Student
 
-  def initialize(name, cohort)
-    @name = name
-    @cohort = cohort
+  def initialize(input_name, input_cohort)
+    @name = input_name
+    @cohort = input_cohort
   end
 
   def get_name()
@@ -11,6 +11,14 @@ class Student
 
   def get_cohort()
     return @cohort
+  end
+
+  def set_name(new_name)
+    @name = new_name
+  end
+
+  def set_cohort(new_cohort)
+    @cohort = new_cohort
   end
 
 end
