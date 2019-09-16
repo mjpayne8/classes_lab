fa68aaa1ef75467b607046d5765de47034cc24d9
mjpayne8 <markpayne@Marks-MacBook-Air.local>
2019-09-16 14:37:54 +0100

    getter and setter methods refactored using attr_reader and attr_accessor

--- specs/team_spec.rb
+++ specs/team_spec.rb
@@ -6,23 +6,23 @@ class TestTeam < MiniTest::Test
 
 def test_get_team_name__returns_watford
   sports_team = Team.new("Watford",["Gomez", "Sarr"],"Flores")
-  assert_equal("Watford", sports_team.get_team_name())
+  assert_equal("Watford", sports_team.team_name())
 end
 
 def test_get_players__returns_gomez_sarr
   sports_team = Team.new("Watford",["Gomez", "Sarr"],"Flores")
-  assert_equal(["Gomez", "Sarr"], sports_team.get_players())
+  assert_equal(["Gomez", "Sarr"], sports_team.players())
 end
 
 def test_get_coach__returns_flores
   sports_team = Team.new("Watford",["Gomez", "Sarr"],"Flores")
-  assert_equal("Flores", sports_team.get_coach())
+  assert_equal("Flores", sports_team.coach())
 end
 
-def test_set_coach__returns_flores
+def test_set_coach__returns_gracia
   sports_team = Team.new("Watford",["Gomez", "Sarr"],"Flores")
-  sports_team.set_coach("Gracia")
-  assert_equal("Gracia", sports_team.get_coach())
+  sports_team.coach = "Gracia"
+  assert_equal("Gracia", sports_team.coach())
 end
 
 end
