cac7629b4db578446bfcd79d56a34567c20653ef
mjpayne8 <markpayne@Marks-MacBook-Air.local>
2019-09-16 13:48:29 +0100

    setter tests added

--- specs/student_spec.rb
+++ specs/student_spec.rb
@@ -13,4 +13,17 @@ class TestStudentClass < MiniTest::Test
     codeclan_student = Student.new("Mark", "E34")
     assert_equal("E34", codeclan_student.get_cohort())
   end
+
+  def test_set_name__test_returns_gary
+    codeclan_student = Student.new("Mark", "E34")
+    codeclan_student.set_name("Gary")
+    assert_equal("Gary", codeclan_student.get_name())
+  end
+
+  def test_set_cohort__test_returns_E33
+    codeclan_student = Student.new("Mark", "E34")
+    codeclan_student.set_cohort("E33")
+    assert_equal("E33", codeclan_student.get_cohort())
+  end
+
 end
