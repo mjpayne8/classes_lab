765003496f5145aa7bdf7ead1095512400fcd1ba
mjpayne8 <markpayne@Marks-MacBook-Air.local>
2019-09-16 14:32:22 +0100

    tests for setter functions added

--- specs/team_spec.rb
+++ specs/team_spec.rb
@@ -19,4 +19,22 @@ def test_get_coach__returns_flores
   assert_equal("Flores", sports_team.get_coach())
 end
 
+def test_set_team_name__returns_hornets
+  sports_team = Team.new("Watford",["Gomez", "Sarr"],"Flores")
+  sports_team.set_team_name("Hornets")
+  assert_equal("Watford", sports_team.get_team_name())
+end
+
+def test_set_players__returns_foster_cleverley
+  sports_team = Team.new("Watford",["Gomez", "Sarr"],"Flores")
+  sports_team.set_players(["Foster", "Cleverley"])
+  assert_equal(["Foster", "Cleverley"], sports_team.get_players())
+end
+
+def test_set_coach__returns_flores
+  sports_team = Team.new("Watford",["Gomez", "Sarr"],"Flores")
+  sports_team.set_coach("Gracia")
+  assert_equal("Flores", sports_team.get_coach())
+end
+
 end
